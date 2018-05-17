f5432537af8864167de5322b709d071102ddf49a
Mitchell <mitchell@Oberon>
2018-05-17 13:28:30 -0400

    Added database files

--- Quote/src/quote/Quote.java
+++ Quote/src/quote/Quote.java
@@ -107,7 +107,7 @@ public class Quote {
 
     private static final String DB_DRIVER = "org.h2.Driver";
     private static final String DB_CONNECTION = "jdbc:h2:" + GetLocation("QuotesDatabase");
-    private static final String DB_USER = "";
+    private static final String DB_USER = "sa";
     private static final String DB_PASSWORD = "";
 
     // H2 SQL Prepared Statement Example
