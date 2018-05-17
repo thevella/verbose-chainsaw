f6f0244a01ff1b606c2df682fb895d90b520982e
Mitchell <mitchell@Oberon>
2018-05-17 13:34:38 -0400

    Removed withPreparedStatement() method that will not be used

--- Quote/src/quote/Quote.java
+++ Quote/src/quote/Quote.java
@@ -110,47 +110,6 @@ public class Quote {
     private static final String DB_USER = "sa";
     private static final String DB_PASSWORD = "";
 
-    // H2 SQL Prepared Statement Example
-    private static void insertWithPreparedStatement() throws SQLException {
-        Connection connection = getDBConnection();
-        PreparedStatement createPreparedStatement = null;
-        PreparedStatement insertPreparedStatement = null;
-        PreparedStatement selectPreparedStatement = null;
-
-        String CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))";
-        String InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)";
-        String SelectQuery = "select * from PERSON";
-        try {
-            connection.setAutoCommit(false);
-
-            createPreparedStatement = connection.prepareStatement(CreateQuery);
-            createPreparedStatement.executeUpdate();
-            createPreparedStatement.close();
-
-            insertPreparedStatement = connection.prepareStatement(InsertQuery);
-            insertPreparedStatement.setInt(1, 1);
-            insertPreparedStatement.setString(2, "Jose");
-            insertPreparedStatement.executeUpdate();
-            insertPreparedStatement.close();
-
-            selectPreparedStatement = connection.prepareStatement(SelectQuery);
-            ResultSet rs = selectPreparedStatement.executeQuery();
-            System.out.println("H2 Database inserted through PreparedStatement");
-            while (rs.next()) {
-                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
-            }
-            selectPreparedStatement.close();
-
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Exception Message " + e.getLocalizedMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            connection.close();
-        }
-    }
-
     // H2 SQL Statement Example
     private static void insertWithStatement() throws SQLException {
         Connection connection = getDBConnection();
