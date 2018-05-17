14e524f6a841ee5c0e94d2e7b7a20d6eb5d44d4d
Coles Laptop <Coles Laptop@10.147.40.185>
2018-05-17 13:58:21 -0400

    got the search button to do something

--- Quote/src/quote/UserInterfaec.java
+++ Quote/src/quote/UserInterfaec.java
@@ -16,6 +16,8 @@ public class UserInterfaec extends javax.swing.JFrame {
      */
     public UserInterfaec() {
         initComponents();
+        
+        
     }
 
     /**
@@ -27,21 +29,106 @@ public class UserInterfaec extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        SearchButton = new javax.swing.JRadioButton();
+        AddButton = new javax.swing.JRadioButton();
+        RemoveButton = new javax.swing.JRadioButton();
+        AuthoButton = new javax.swing.JRadioButton();
+        QuoteButton = new javax.swing.JRadioButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        OutScreen = new javax.swing.JTextPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        SearchWord = new javax.swing.JEditorPane();
+        jToggleButton1 = new javax.swing.JToggleButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        SearchButton.setText("Search");
+
+        AddButton.setText("Add");
+
+        RemoveButton.setText("Remove");
+
+        AuthoButton.setText("Author");
+        AuthoButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AuthoButtonActionPerformed(evt);
+            }
+        });
+
+        QuoteButton.setText("Quote");
+
+        jScrollPane1.setViewportView(OutScreen);
+
+        jScrollPane2.setViewportView(SearchWord);
+
+        jToggleButton1.setText("Search");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(SearchButton)
+                            .addComponent(RemoveButton)
+                            .addComponent(AddButton))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(35, 35, 35)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(QuoteButton)
+                                    .addComponent(AuthoButton)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(122, 122, 122)
+                                .addComponent(jToggleButton1)
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(SearchButton)
+                            .addComponent(AuthoButton))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(AddButton)
+                            .addComponent(QuoteButton))
+                        .addGap(18, 18, 18)
+                        .addComponent(RemoveButton)
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jToggleButton1))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void AuthoButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AuthoButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_AuthoButtonActionPerformed
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+OutScreen.setText("new string");
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +166,15 @@ public class UserInterfaec extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JRadioButton AddButton;
+    private javax.swing.JRadioButton AuthoButton;
+    private javax.swing.JTextPane OutScreen;
+    private javax.swing.JRadioButton QuoteButton;
+    private javax.swing.JRadioButton RemoveButton;
+    private javax.swing.JRadioButton SearchButton;
+    private javax.swing.JEditorPane SearchWord;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
