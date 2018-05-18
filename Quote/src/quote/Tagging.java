06c2736a677f4f8136ffecd250af18d034b8c1ec
390715 <390715@STS-849.SMCDSB.ADS>
2018-05-18 13:43:18 -0400

    The Tagging Section is Added

---
+++ Quote/src/quote/Tagging.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quote;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 390715
+ */
+public class Tagging {
+    public ArrayList<String> happyL = new ArrayList<String>();
+    
+    
+}
