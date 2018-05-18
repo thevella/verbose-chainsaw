eb11c971ac61a9baa8fcd8e9dfdf85adfb5c3a9e
Coles Laptop <Coles Laptop@10.147.40.185>
2018-05-18 10:00:24 -0400

    added sample code for visabilty of pannels

--- Quote/src/quote/UserInterfaec.java
+++ Quote/src/quote/UserInterfaec.java
@@ -8,6 +8,60 @@ package quote;
 /**
  *
  * @author Coles Laptop
+ Sample code for visability 
+ * import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class togglepanel {
+
+    public static void main(String[] args) {
+
+        final JFrame frame = new JFrame();
+        final JPanel panel1 = new JPanel();
+        final JPanel panel2 = new JPanel();
+        JButton button1 = new JButton("previous frame!");
+        JButton button2 = new JButton("next frame");
+
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setVisible(true);
+        frame.setSize(600, 400);
+        frame.add(panel1);
+        frame.add(panel2);
+
+        panel1.add(button2);
+        panel1.setVisible(true);
+
+        panel2.add(button1);
+        panel2.setVisible(false);
+
+        button1.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+
+                panel1.setVisible(true);
+                panel2.setVisible(false);
+
+            }
+        });
+
+
+        button2.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+
+                panel1.setVisible(false);
+                panel2.setVisible(true);
+            }
+        });
+    }
+}
+ 
+ 
+ 
  */
 public class UserInterfaec extends javax.swing.JFrame {
 
