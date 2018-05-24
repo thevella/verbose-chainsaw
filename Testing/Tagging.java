28dd9cb660fb7d631abdf63666052a3430f306b5
Mitchell <mitchell@Oberon>
2018-05-24 13:36:47 -0400

    Copied to testing

---
+++ Testing/Tagging.java
@@ -0,0 +1,295 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quote;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 390715
+ */
+/*public class Tagging {
+     ArrayList<String> openL = new ArrayList<String>();
+        openL.add("Understanding");
+        openL.add("Confident");
+        openL.add("Reliable");
+        openL.add("Easy");
+        openL.add("Amazed");
+        openL.add("Free");
+        openL.add("Sympathetic");
+        openL.add("Interested");
+        openL.add("Satisfied");
+        openL.add("Receptive");
+        openL.add("Accepting");
+        openL.add("Kind");
+
+        ArrayList<String> happyL = new ArrayList<String>();
+        happyL.add("Great");
+        happyL.add("Gay");
+        happyL.add("Joyous");
+        happyL.add("Lucky");
+        happyL.add("Fortunate");
+        happyL.add("Delighted");
+        happyL.add("Overjoyed");
+        happyL.add("Gleeful");
+        happyL.add("Thankful");
+        happyL.add("Important");
+        happyL.add("Festive");
+        happyL.add("Ecstatic");
+        happyL.add("Satisfied");
+        happyL.add("Glad");
+        happyL.add("Cheerful");
+        happyL.add("Sunny");
+        happyL.add("Merry");
+        happyL.add("Elated");
+        happyL.add("Jubilant");
+
+        ArrayList<String> aliveL = new ArrayList<String>();
+        aliveL.add("Playful");
+        aliveL.add("Courageous");
+        aliveL.add("Energetic");
+        aliveL.add("Liberated");
+        aliveL.add("Optimistic");
+        aliveL.add("Provocative");
+        aliveL.add("Impulsive");
+        aliveL.add("Free");
+        aliveL.add("Frisky");
+        aliveL.add("Animated");
+        aliveL.add("Spirited");
+        aliveL.add("Thrilled");
+        aliveL.add("Wonderful");
+
+        ArrayList<String> goodL = new ArrayList<String>();
+        goodL.add("Calm");
+        goodL.add("Peaceful");
+        goodL.add("At Ease");
+        goodL.add("Comfortable");
+        goodL.add("Pleased");
+        goodL.add("Encouraged");
+        goodL.add("Clever");
+        goodL.add("Surprised");
+        goodL.add("Content");
+        goodL.add("Quiet");
+        goodL.add("Certain");
+        goodL.add("Relaxed");
+        goodL.add("Serene");
+        goodL.add("Free and Easy");
+        goodL.add("Bright");
+        goodL.add("Blessed");
+        goodL.add("Reassured");
+
+        ArrayList<String> loveL = new ArrayList<String>();
+        loveL.add("Loving");
+        loveL.add("Considerate");
+        loveL.add("Affectionate");
+        loveL.add("Sensitive");
+        loveL.add("Tender");
+        loveL.add("Devoted");
+        loveL.add("Attracted");
+        loveL.add("Passionate");
+        loveL.add("Admiration");
+        loveL.add("Warm");
+        loveL.add("Touched");
+        loveL.add("Sympathy");
+        loveL.add("Close");
+        loveL.add("Loved");
+        loveL.add("Comforted");
+        loveL.add("Drawn Towards");
+
+        ArrayList<String> interestedL = new ArrayList<String>();
+        interestedL.add("Concerned");
+        interestedL.add("Affected");
+        interestedL.add("Fascinated");
+        interestedL.add("Intrigued");
+        interestedL.add("Absorbed");
+        interestedL.add("Inquisitive");
+        interestedL.add("Nosy");
+        interestedL.add("Snoopy");
+        interestedL.add("Engrossed");
+        interestedL.add("Curious");
+
+        ArrayList<String> positiveL = new ArrayList<String>();
+        positiveL.add("Eager Keen");
+        positiveL.add("Earnest Intent");
+        positiveL.add("Anxious Inspired");
+        positiveL.add("Determined Excited");
+        positiveL.add("Enthusiastic Bold");
+        positiveL.add("Brave Daring");
+        positiveL.add("Challenged Optimistic");
+        positiveL.add("Re - Enforced");
+        positiveL.add("Confident Hopeful");
+
+        ArrayList<String> strongL = new ArrayList<String>();
+        strongL.add("Impulsive");
+        strongL.add("Free");
+        strongL.add("Sure");
+        strongL.add("Certain");
+        strongL.add("Rebellious");
+        strongL.add("Unique");
+        strongL.add("Dynamic");
+        strongL.add("Tenacious");
+        strongL.add("Hardy");
+        strongL.add("Secure");
+
+        ArrayList<String> angryL = new ArrayList<String>();
+        angryL.add("Irritated");
+        angryL.add("Enraged");
+        angryL.add("Hostile");
+        angryL.add("Insulting");
+        angryL.add("Sore");
+        angryL.add("Annoyed");
+        angryL.add("Upset");
+        angryL.add("Hateful");
+        angryL.add("Unpleasant");
+        angryL.add("Offensive");
+        angryL.add("Bitter");
+        angryL.add("Aggressive");
+        angryL.add("Resentful");
+        angryL.add("Inflamed");
+        angryL.add("Provoked");
+        angryL.add("Incensed");
+        angryL.add("Infuriated");
+        angryL.add("Cross");
+        angryL.add("Working Up");
+        angryL.add("Boiling");
+        angryL.add("Fuming");
+        angryL.add("Indignant");
+       
+        ArrayList<String> depressedL = new ArrayList<String>();
+        depressedL.add("Lousy");
+        depressedL.add("Disappoinnted");
+        depressedL.add("Discouraged");
+        depressedL.add("Ashamed");
+        depressedL.add("Powerless");
+        depressedL.add("Diminished");
+        depressedL.add("Gulity");
+        depressedL.add("Dissatisfied");
+        depressedL.add("Miserable");
+        depressedL.add("Detestable");
+        depressedL.add("Repugnant");
+        depressedL.add("Dispicable");
+        depressedL.add("Disgusting");
+        depressedL.add("Abominable");
+        depressedL.add("Terrible");
+        depressedL.add("In Despair");
+        depressedL.add("Sulky");
+        depressedL.add("Bad");
+        depressedL.add("A Sense of Loss");
+        
+        ArrayList<String> confusedL = new ArrayList<String>();
+        confusedL.add("Upset");
+        confusedL.add("Doubtful");
+        confusedL.add("Uncertain");
+        confusedL.add("Indecisive");
+        confusedL.add("Perplexed");
+        confusedL.add("Embarrassed");
+        confusedL.add("Hesitant");
+        confusedL.add("Shy");
+        confusedL.add("Stupefied");
+        confusedL.add("Disillusioned");
+        confusedL.add("Unbelieving");
+        confusedL.add("Skeptical");
+        confusedL.add("Distrustful");
+        confusedL.add("Misgiving");
+        confusedL.add("Lost");
+        confusedL.add("Unsure");
+        confusedL.add("Uneasy");
+        confusedL.add("Pessemistic");
+        confusedL.add("Tense");
+        
+        ArrayList<String> helplessL = new ArrayList<String>();
+        helplessL.add("Incapable");
+        helplessL.add("Alone");
+        helplessL.add("Paralyzed");
+        helplessL.add("Fatigued");
+        helplessL.add("Useless");
+        helplessL.add("Inferior");
+        helplessL.add("Vulnerable");
+        helplessL.add("Empty");
+        helplessL.add("Forced");
+        helplessL.add("Hesitant");
+        helplessL.add("Dispair");
+        helplessL.add("Frustrated");
+        helplessL.add("Distressed");
+        helplessL.add("Woeful");
+        helplessL.add("Pathetic");
+        helplessL.add("Tragic");
+        helplessL.add("In A Stew");
+        helplessL.add("Dominated");
+        
+        ArrayList<String> indifferentL = new ArrayList<String>();
+        indifferentL.add("Insensitive");
+        indifferentL.add("Dull");
+        indifferentL.add("Nonchalant");
+        indifferentL.add("Neutral");
+        indifferentL.add("Reserved");
+        indifferentL.add("Weary");
+        indifferentL.add("Bored");
+        indifferentL.add("Preoccupied");
+        indifferentL.add("Cold");
+        indifferentL.add("Disinterested");
+        indifferentL.add("Lifeless");
+        
+        ArrayList<String> afraidL = new ArrayList<String>();
+        afraidL.add("Fearful");
+        afraidL.add("Terrified");
+        afraidL.add("Suspicious");
+        afraidL.add("Anxious");
+        afraidL.add("Alarmed");
+        afraidL.add("Panic");
+        afraidL.add("Nervous");
+        afraidL.add("Scared");
+        afraidL.add("Worried");
+        afraidL.add("Frightened");
+        afraidL.add("Timid");
+        afraidL.add("Shaky");
+        afraidL.add("Restless");
+        afraidL.add("Doubtful");
+        afraidL.add("Threatened");
+        afraidL.add("Cowardly");
+        afraidL.add("Quaking");
+        afraidL.add("Menaced");
+        afraidL.add("Wary");
+        
+        ArrayList<String> hurtL = new ArrayList<String>();
+        hurtL.add("Crushed");
+        hurtL.add("Tormented");
+        hurtL.add("Deprived");
+        hurtL.add("Pained");
+        hurtL.add("Tortured");
+        hurtL.add("Dejected");
+        hurtL.add("Rejected");
+        hurtL.add("Injured");
+        hurtL.add("Offended");
+        hurtL.add("Afflicted");
+        hurtL.add("Aching");
+        hurtL.add("Victimized");
+        hurtL.add("Heartbroken");
+        hurtL.add("Agonized");
+        hurtL.add("Appalled");
+        hurtL.add("Humiliated");
+        hurtL.add("Wronged");
+        hurtL.add("Alienated");
+        
+        ArrayList<String> sadL = new ArrayList<String>();
+         sadL.add("Tearful");
+         sadL.add("Sorrowful");
+         sadL.add("Pained");
+         sadL.add("Grief");
+         sadL.add("Anguish");
+         sadL.add("Desolate");
+         sadL.add("Desperate");
+         sadL.add("Pessimistic");
+         sadL.add("Unhappy");
+         sadL.add("Lonely");
+         sadL.add("Grieved");
+         sadL.add("Mournful");
+         sadL.add("Dismayed");
+         
+
+
+}
+*/
