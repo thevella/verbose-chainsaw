bec83b539322fe39a7342c637c04193199b22bac
Coles Laptop <Coles Laptop@10.147.40.185>
2018-05-28 14:57:06 -0400

    Added the ability to add quotes and authors

--- Quote/src/quote/UserInterfaec.java
+++ Quote/src/quote/UserInterfaec.java
@@ -68,14 +68,13 @@ public class UserInterfaec extends javax.swing.JFrame {
         Output1 = new javax.swing.JList<>();
         jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
+        Button_Add = new javax.swing.JButton();
         jScrollPane5 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        Author_Add = new javax.swing.JTextArea();
         jScrollPane6 = new javax.swing.JScrollPane();
-        jTextArea3 = new javax.swing.JTextArea();
+        Quote_Add = new javax.swing.JTextArea();
         jScrollPane7 = new javax.swing.JScrollPane();
-        jTextArea4 = new javax.swing.JTextArea();
+        Tags_Add = new javax.swing.JTextArea();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
 
@@ -313,21 +312,24 @@ public class UserInterfaec extends javax.swing.JFrame {
 
         jLabel1.setText("Author");
 
-        jButton1.setText("Add");
+        Button_Add.setText("Add");
+        Button_Add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Button_AddActionPerformed(evt);
+            }
+        });
 
-        jLabel4.setText("All Author Entries Should Be Formated \"--Author\".");
+        Author_Add.setColumns(20);
+        Author_Add.setRows(5);
+        jScrollPane5.setViewportView(Author_Add);
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane5.setViewportView(jTextArea2);
+        Quote_Add.setColumns(20);
+        Quote_Add.setRows(5);
+        jScrollPane6.setViewportView(Quote_Add);
 
-        jTextArea3.setColumns(20);
-        jTextArea3.setRows(5);
-        jScrollPane6.setViewportView(jTextArea3);
-
-        jTextArea4.setColumns(20);
-        jTextArea4.setRows(5);
-        jScrollPane7.setViewportView(jTextArea4);
+        Tags_Add.setColumns(20);
+        Tags_Add.setRows(5);
+        jScrollPane7.setViewportView(Tags_Add);
 
         jLabel2.setText("Quote");
 
@@ -337,30 +339,26 @@ public class UserInterfaec extends javax.swing.JFrame {
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(44, 44, 44))
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGap(316, 316, 316)
                         .addComponent(jLabel1))
                     .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addGap(232, 232, 232)
-                        .addComponent(jLabel4))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGap(175, 175, 175)
                         .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jScrollPane7, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jScrollPane7, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
+                                .addComponent(Button_Add))))
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGap(321, 321, 321)
                         .addComponent(jLabel2))
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGap(328, 328, 328)
                         .addComponent(jLabel3)))
-                .addContainerGap(221, Short.MAX_VALUE))
+                .addGap(81, 81, 81))
             .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jPanel3Layout.createSequentialGroup()
                     .addGap(176, 176, 176)
@@ -372,15 +370,16 @@ public class UserInterfaec extends javax.swing.JFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(15, 15, 15)
                 .addComponent(jLabel1)
-                .addGap(14, 14, 14)
-                .addComponent(jButton1)
-                .addGap(78, 78, 78)
-                .addComponent(jLabel4)
-                .addGap(18, 18, 18)
+                .addGap(147, 147, 147)
                 .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(4, 4, 4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(4, 4, 4))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addComponent(Button_Add)
+                        .addGap(41, 41, 41)))
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -590,6 +589,17 @@ public class UserInterfaec extends javax.swing.JFrame {
         
         
     }//GEN-LAST:event_Author_RemoveActionPerformed
+
+    private void Button_AddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Button_AddActionPerformed
+         Quote aa= new Quote();
+         
+         
+        try {
+            aa.insertQuotes(Author_Add.getText(),Quote_Add.getText(),Tags_Add.getText());
+        } catch (SQLException ex) {
+            Logger.getLogger(UserInterfaec.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_Button_AddActionPerformed
     
 
     /**
@@ -630,23 +640,25 @@ public class UserInterfaec extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem Auth_Search;
     private javax.swing.JList<String> AuthorDisplay;
+    private javax.swing.JTextArea Author_Add;
     private javax.swing.JMenuItem Author_Remove;
     private javax.swing.JRadioButton Author_Search;
+    private javax.swing.JButton Button_Add;
     private javax.swing.JList<String> Output1;
     private javax.swing.JPopupMenu PopUp;
     private javax.swing.JPopupMenu QuotePopOut;
+    private javax.swing.JTextArea Quote_Add;
     private javax.swing.JRadioButton Quote_Search;
     private javax.swing.JMenuItem Remove;
     private javax.swing.JButton SearchButton;
     private javax.swing.JTextArea SearchTerm;
+    private javax.swing.JTextArea Tags_Add;
     private javax.swing.JRadioButton Tags_Search;
     private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JButton jButton1;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
@@ -663,8 +675,5 @@ public class UserInterfaec extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane7;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTextArea jTextArea2;
-    private javax.swing.JTextArea jTextArea3;
-    private javax.swing.JTextArea jTextArea4;
     // End of variables declaration//GEN-END:variables
 }
