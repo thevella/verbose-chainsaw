f68f4caaa6ef80faa99f40a03bd2284ea4f40f81
Mitchell <mitchell@Oberon>
2018-05-28 12:26:22 -0400

    Messing with database, fixed some names

---
+++ Testing/JavascriptToRemoveWiki.js
@@ -0,0 +1,19 @@
+/////// Only here to next comment
+javascript:function a (){
+    document.body.innerHTML=document.body.innerHTML.replace(/<sup\b[^>]*>(.*?)<\/sup>/gi, "" );
+    document.body.innerHTML=document.body.innerHTML.replace(/<a\b[^>]*>/gi, "" );
+    document.body.innerHTML=document.body.innerHTML.replace(/<\/a>/gi, "" );
+    return;
+};
+
+a();
+
+//////
+javascript:function a (){
+    document.body.innerHTML=document.body.innerHTML.replace(/<a\b[^>]*>/gi, "" );
+    document.body.innerHTML=document.body.innerHTML.replace(/<\/a>/gi, "" );
+    return;
+};
+
+a();
+<a href="/wiki/Joy_Davidman" title="Joy Davidman">Joy Davidman</a>
